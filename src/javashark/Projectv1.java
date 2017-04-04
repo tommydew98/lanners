a8246ad107d35abf75108e96a055b8bfa36cc314
tommydew98 <blueberryx6445@gmail.com>
2017-04-03 18:05:22 -0700

    second commit

--- src/javashark/Projectv1.java
+++ src/javashark/Projectv1.java
@@ -68,8 +68,12 @@ public class Projectv1 {
          JFlowMap superFlowMap = new JFlowMap();
 
          ArrayList<Integer> packSize = new ArrayList<>();
+         ArrayList<Long> retransmissions = new ArrayList<>();
          pcap.loop(Pcap.LOOP_INFINITE, new JPacketHandler<StringBuilder>() {
              final Ip4 ipv4 = new Ip4();
+             final Tcp tcp = new Tcp();
+             Long ack = Long.valueOf(1);
+             Long seq = Long.valueOf(1);
              @Override
              public void nextPacket(JPacket jPacket, StringBuilder stringBuilder) {
                   if (jPacket.hasHeader(Ip4.ID)){
@@ -81,16 +85,35 @@ public class Projectv1 {
                       System.out.println(jPacket.getTotalSize()+" bytes");
 
                  }
+                 if (jPacket.hasHeader(tcp)){
+
+                     jPacket.getHeader(tcp);
+
+                     if(ack.equals(tcp.ack()) && seq.equals(tcp.seq())){
+                         //add frame# to list
+                        retransmissions.add(jPacket.getFrameNumber());
+                     } else {
+                         ack = tcp.ack();
+                         seq = tcp.seq();
+                     }
+
+                     System.out.println("ACK "+tcp.ack());
+                     System.out.println("SEQ "+tcp.seq());
+
+
+                 }
              }
          }, mrString);
          //System.out.printf("superFlowMap::%s%n", superFlowMap);
          System.out.println("Total # of packets: "+packSize.size());
          System.out.println(packSize);
+         System.out.println(retransmissions);
          int total=0;
          for(int i : packSize){
              total= total+i;
          }
          System.out.println("Average Packet Size: "+total/packSize.size()+" bytes");
+         System.out.println("Number of Retransmissions: "+retransmissions.size());
 
 //Pcap.LOOP_INFINITE
          pcap.close();
