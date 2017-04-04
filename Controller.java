c14a5f059f2abe2929cacb20b41d364f1ddbf6db
yusukeito15 <yusukeito15@gmail.com>
2017-04-03 19:53:44 -0700

    GUI can read

--- Controller.java
+++ Controller.java
@@ -1,13 +1,31 @@
 package javashark;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.*;
+import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.*;
 
-public class Controller {
+import java.awt.*;
+import java.awt.Button;
+import java.awt.Window;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Controller{
     @FXML
     LineChart<String, Number> lineChart;
+    private Desktop desktop = Desktop.getDesktop();
 
     public void btn(ActionEvent event){
         XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
@@ -15,5 +33,43 @@ public class Controller {
         series.getData().add(new XYChart.Data<String, Number>("Test2", 200));
         series.getData().add(new XYChart.Data<String, Number>("Test3", 300));
         lineChart.getData().add(series);
+    }
+
+
+   public void browseButton(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+//        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("PCAP files (*.pcap)");
+//        fileChooser.getExtensionFilters().add(extFilter);
+
+        File file = fileChooser.showOpenDialog(new Stage());
+       
+        if (file != null) {
+           System.out.print(file.getPath());
+        }
+   }
+
+
+    private static void configureFileChooser(final FileChooser fileChooser){
+        fileChooser.setTitle("View Pictures");
+        fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home"))
+        );
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All", "*.*"),
+                new FileChooser.ExtensionFilter("PCAP", "*.pcap")
+        );
+    }
+
+    private void openFile(File file) {
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            Logger.getLogger(
+                    FileChooserSample.class.getName()).log(
+                    Level.SEVERE, null, ex
+            );
+        }
     }
 }
