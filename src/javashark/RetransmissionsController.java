44564918214bd7c50b4e1c8001448595d750db45
tommydew98 <blueberryx6445@gmail.com>
2017-04-12 16:42:11 -0700

    fml

--- src/javashark/RetransmissionsController.java
+++ src/javashark/RetransmissionsController.java
@@ -1,24 +1,30 @@
 package javashark;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+
+import java.net.URL;
+
+import java.util.ResourceBundle;
 
 /**
  * Created by Yusuke on 2017-04-11.
  */
-public class RetransmissionsController {
-    public void homeButton(ActionEvent event) throws IOException {
-        System.out.println("HOME");
-        Parent homeParent = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Scene homeScene = new Scene(homeParent);
-        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        app_stage.setScene(homeScene);
-        app_stage.show();
+public class RetransmissionsController implements Initializable {
+
+    @FXML
+    private PieChart rePie;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("test");
+
+
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(new PieChart.Data("Retransmissions", 20), new PieChart.Data("Total Packets",1000));
+        rePie.setData(pieChartData);
+
     }
 }
