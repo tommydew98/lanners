f66ccf00584df7a2a5a968df836ad62bd0dbda4a
tommydew98 <blueberryx6445@gmail.com>
2017-04-12 20:17:53 -0700

    bar grpah

--- src/javashark/DNSController.java
+++ src/javashark/DNSController.java
@@ -4,10 +4,16 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -15,11 +21,23 @@ import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Yusuke on 2017-04-11.
  */
-public class DNSController implements ControllerInterface{
+public class DNSController implements ControllerInterface, Initializable{
+
+    @FXML
+    private BarChart<String, Integer> barGraph;
+
+    @FXML
+    private CategoryAxis x;
+
+    @FXML
+    private NumberAxis y;
+
     public void homeButton(ActionEvent event) throws IOException {
         System.out.println("HOME");
         Stage stage = new Stage();
@@ -38,5 +56,15 @@ public class DNSController implements ControllerInterface{
         scene.setFill(Color.TRANSPARENT); //Makes scene background transparent
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        XYChart.Series series = new XYChart.Series<>();
+
+        series.getData().add(new XYChart.Data("Test", 20));
+
+        barGraph.getData().addAll(series);
+
     }
 }
